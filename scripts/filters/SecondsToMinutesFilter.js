8feb48d3f0b7dbb88c1a52407c295ea2ef38df87
Josh Panka <joshpanka@Joshs-MacBook-Pro.local>
2017-03-07 01:42:22 -0600

    Add comments to SecondsToMinutesFilter
    Add comments to stated filter

--- scripts/filters/SecondsToMinutesFilter.js
+++ scripts/filters/SecondsToMinutesFilter.js
@@ -5,7 +5,12 @@
         .module("uberTripEstimater")
         .filter("secondsToMinutes", secondsToMinutes);
 
-
+    /**
+     * Converts number of seconds to minutes.
+     *
+     * @param seconds - given number of seconds
+     * @return number of minutes from seconds
+     */
     function secondsToMinutes() {
         return function(seconds) {
             return seconds/60;
